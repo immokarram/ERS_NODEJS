0309f3744f2c7f95a1ba43bdace20af799ef18be
Mokarram Ali <88176270+immokarram@users.noreply.github.com>
2023-07-05 06:29:05 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://0.0.0.0/employeReviewSystem');
+const DB = 'mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority'
+
+// const DB = 'mongodb+srv://placement:eJ6gE4JuwEVFMvfE@cluster0.xge3fgc.mongodb.net/?retryWrites=true&w=majority'
+
+
+// These set of line can be written in async await fashion, but I have followed the documentation. 
+mongoose.connect(DB).then(()=>{
+     console.log('connection successful');
+ }).catch((err) => console.log("no connection " + err));
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open',  function(){
+     console.log('Connected to Database :: MongoDB');
+});
+
+ 
+module.exports = db;  
